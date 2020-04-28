f39a50087fa91e1fcee5ee7ee149e3974ef35354
pattar113 <pattar.dilm@gmail.com>
2020-04-28 09:39:46 -0700

    ArrayListMethods

--- src/day30_ArrayList/ArrayList_Methods.java
+++ src/day30_ArrayList/ArrayList_Methods.java
@@ -32,8 +32,8 @@ list2.add("A");
 list2.add("D");
 list2.add(1,"C");
 list2.add(1,"B");// "A","B","C","D"
-list2.set(2,"E");//"A","B","E","D"
-list2.set(3,"F");//"A","B","E","F"
+list2.set(2,"E");//"A","B","E","D"  replacing index2
+list2.set(3,"F");//"A","B","E","F"   replacing index3
 
         System.out.println(list2);
         System.out.println("==================================");
@@ -45,9 +45,31 @@ list2.set(3,"F");//"A","B","E","F"
         list3.add(3);
         list3.add(4);
         list3.add(5);
+       //list3 =  12345
+
+        list3.remove(3);//removing index 3
+       // list3 = 1,2,3,5
+
+        Integer a = 1;
+        list3.remove(a);// removing object 1
+        System.out.println(list3);
 
 
-        System.out.println(list3);// 1,2,3,4,5
+        ArrayList<Integer>list4 = new ArrayList<>();
+
+        list4.add(10);
+        list4.add(20);
+        list4.add(30);
+       // 10,20,30
+       // list4.remove(1);// removing 20;
+
+      Integer c = 20;
+        list4.remove(c);     // removing 20;  also can be assigned to boolean, if the object is there will be return true
+
+
+        System.out.println(list4);
+
+
 
 
 
